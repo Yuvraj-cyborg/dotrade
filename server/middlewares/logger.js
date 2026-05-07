c3e0e6123a591f492f9ccfa064da836a90c8beb0
Yuvraj <yuvrajbiswalofficial@gmail.com>
2026-05-08 01:43:51 +0530

    stage-1 migration from JS->Rust

--- server/middlewares/logger.js
+++
@@ -1,8 +0,0 @@
-function logger(req, res, next) {
-  console.log(
-    `${new Date().toISOString()} ${req.url} ${req.method} ${JSON.stringify(req.body)}`,
-  );
-  next();
-}
-
-export default logger;
--- server/utils/symbols.js
+++
@@ -1,5 +0,0 @@
-const SYMBOLS = ["BTC", "ETH", "SOL", "AVAX"];
-
-const isValidSymbol = (s) => SYMBOLS.includes(s);
-
-export { SYMBOLS, isValidSymbol };
